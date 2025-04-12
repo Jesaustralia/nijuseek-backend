55ac2ecbfe15d1e0a79ce96671fce724b8f9fbfb
Jesaustralia <jesaustralia@gmail.com>
2025-04-12 05:20:22 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 import random
 import urllib.parse
 
 app = Flask(__name__)
+CORS(app)
 
 # Simulated user agents for scraping
 user_agents = [
